7ccb7a24415e3e72aed28d129a070f965cc614d7
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2020-10-31 22:23:40 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,324 @@
+$(document).ready(function () {
+
+
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+    });
+
+    $(".header").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
+    $(".after").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+    });
+});
+
+const plus = document.querySelectorAll('.item-answer__plus');
+
+$(plus.forEach(function (el) {
+    $(el).click(function () {
+        $(this.parentNode).toggleClass('active');
+    });
+}))
+
+
+
+"use strict";
+
+(function () {
+    let originalPositions = [];
+    let daElements = document.querySelectorAll('[data-da]');
+    let daElementsArray = [];
+    let daMatchMedia = [];
+    //Заполняем массивы
+    if (daElements.length > 0) {
+        let number = 0;
+        for (let index = 0; index < daElements.length; index++) {
+            const daElement = daElements[index];
+            const daMove = daElement.getAttribute('data-da');
+            if (daMove != '') {
+                const daArray = daMove.split(',');
+                const daPlace = daArray[1] ? daArray[1].trim() : 'last';
+                const daBreakpoint = daArray[2] ? daArray[2].trim() : '767';
+                const daType = daArray[3] === 'min' ? daArray[3].trim() : 'max';
+                const daDestination = document.querySelector('.' + daArray[0].trim())
+                if (daArray.length > 0 && daDestination) {
+                    daElement.setAttribute('data-da-index', number);
+                    //Заполняем массив первоначальных позиций
+                    originalPositions[number] = {
+                        "parent": daElement.parentNode,
+                        "index": indexInParent(daElement)
+                    };
+                    //Заполняем массив элементов 
+                    daElementsArray[number] = {
+                        "element": daElement,
+                        "destination": document.querySelector('.' + daArray[0].trim()),
+                        "place": daPlace,
+                        "breakpoint": daBreakpoint,
+                        "type": daType
+                    }
+                    number++;
+                }
+            }
+        }
+        dynamicAdaptSort(daElementsArray);
+
+        //Создаем события в точке брейкпоинта
+        for (let index = 0; index < daElementsArray.length; index++) {
+            const el = daElementsArray[index];
+            const daBreakpoint = el.breakpoint;
+            const daType = el.type;
+
+            daMatchMedia.push(window.matchMedia("(" + daType + "-width: " + daBreakpoint + "px)"));
+            daMatchMedia[index].addListener(dynamicAdapt);
+        }
+    }
+    //Основная функция
+    function dynamicAdapt(e) {
+        for (let index = 0; index < daElementsArray.length; index++) {
+            const el = daElementsArray[index];
+            const daElement = el.element;
+            const daDestination = el.destination;
+            const daPlace = el.place;
+            const daBreakpoint = el.breakpoint;
+            const daClassname = "_dynamic_adapt_" + daBreakpoint;
+
+            if (daMatchMedia[index].matches) {
+                //Перебрасываем элементы
+                if (!daElement.classList.contains(daClassname)) {
+                    let actualIndex = indexOfElements(daDestination)[daPlace];
+                    if (daPlace === 'first') {
+                        actualIndex = indexOfElements(daDestination)[0];
+                    } else if (daPlace === 'last') {
+                        actualIndex = indexOfElements(daDestination)[indexOfElements(daDestination).length];
+                    }
+                    daDestination.insertBefore(daElement, daDestination.children[actualIndex]);
+                    daElement.classList.add(daClassname);
+                }
+            } else {
+                //Возвращаем на место
+                if (daElement.classList.contains(daClassname)) {
+                    dynamicAdaptBack(daElement);
+                    daElement.classList.remove(daClassname);
+                }
+            }
+        }
+        customAdapt();
+    }
+
+    //Вызов основной функции
+    dynamicAdapt();
+
+    //Функция возврата на место
+    function dynamicAdaptBack(el) {
+        const daIndex = el.getAttribute('data-da-index');
+        const originalPlace = originalPositions[daIndex];
+        const parentPlace = originalPlace['parent'];
+        const indexPlace = originalPlace['index'];
+        const actualIndex = indexOfElements(parentPlace, true)[indexPlace];
+        parentPlace.insertBefore(el, parentPlace.children[actualIndex]);
+    }
+    //Функция получения индекса внутри родителя
+    function indexInParent(el) {
+        var children = Array.prototype.slice.call(el.parentNode.children);
+        return children.indexOf(el);
+    }
+    //Функция получения массива индексов элементов внутри родителя 
+    function indexOfElements(parent, back) {
+        const children = parent.children;
+        const childrenArray = [];
+        for (let i = 0; i < children.length; i++) {
+            const childrenElement = children[i];
+            if (back) {
+                childrenArray.push(i);
+            } else {
+                //Исключая перенесенный элемент
+                if (childrenElement.getAttribute('data-da') == null) {
+                    childrenArray.push(i);
+                }
+            }
+        }
+        return childrenArray;
+    }
+    //Сортировка объекта
+    function dynamicAdaptSort(arr) {
+        arr.sort(function (a, b) {
+            if (a.breakpoint > b.breakpoint) { return -1 } else { return 1 }
+        });
+        arr.sort(function (a, b) {
+            if (a.place > b.place) { return 1 } else { return -1 }
+        });
+    }
+    //Дополнительные сценарии адаптации
+    function customAdapt() {
+        //const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    }
+}());
+
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+
+const timeout = 500;
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault();
+        });
+    }
+}
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        });
+    }
+}
+
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
+        curentPopup.classList.add('open');
+        curentPopup.addEventListener("click", function (e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        });
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove('open');
+        if (doUnlock) {
+            bodyUnLock();
+        }
+    }
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+    if (lockPadding.length > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+function bodyUnLock() {
+    setTimeout(function () {
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = '0px';
+            }
+        }
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open');
+        popupClose(popupActive);
+    }
+});
+
+(function () {
+    // проверяем поддержку
+    if (!Element.prototype.closest) {
+        // реализуем
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+(function () {
+    // проверяем поддержку
+    if (!Element.prototype.matches) {
+        // определяем свойство
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
